12f48ca760357dd04dc12ab294680f5a7e7f317a
Lok3rs <lok3rsss@gmail.com>
2022-03-29 22:11:26 +0200

    fix

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,17 +1,13 @@
-import { Component } from "react";
+import React from "react";
 import { Typography } from "@mui/material";
 
-class MainPage extends Component {
-  render() {
+const MainPage = () =>  {
     return (
-      <div>
         <Typography variant="h3" marginTop="10%" marginLeft="10%">
           Dear user, you can only go to register page:{" "}
           <a href="/register">RegisterPage</a>
         </Typography>
-      </div>
     );
-  }
 }
 
 export default MainPage;
